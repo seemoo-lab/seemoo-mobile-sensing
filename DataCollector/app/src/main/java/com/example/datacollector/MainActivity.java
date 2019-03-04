dc01d467b1fe0c248e9e44f1f2f0e11b3ec6c4ac
Mikhail Fomichev <mfomichev@seemoo.tu-darmstadt.de>
2019-03-04 09:55:00 +0100

    updated app's icon

--- DataCollector/app/src/main/java/com/example/datacollector/MainActivity.java
+++ DataCollector/app/src/main/java/com/example/datacollector/MainActivity.java
@@ -536,7 +536,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        //Fabric.with(this, new Crashlytics());
         ctx = this;
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         preferences.registerOnSharedPreferenceChangeListener(prefListener);
